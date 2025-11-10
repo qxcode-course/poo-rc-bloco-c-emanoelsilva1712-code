3eca244ad1da38cf71e2dc7411baaa6a1a16515e
emanoelsilva1712-code <emanoelsilva1712@gmail.com>
2025-11-09 23:38:31 -0300

    2 horas

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -6,7 +6,7 @@ class Cliente:
         return self.__nome
     
     def __str__(self):
-        return f"Nome: {self.__nome}"
+        return f"{self.__nome}"
 
 class Mercantil:
     def __init__(self, n_caixas: int):
@@ -19,7 +19,7 @@ class Mercantil:
     def __str__(self): 
         caixas = ", ".join([str(x) if x else "-----" for x in self.caixas])
         fila = ", ".join([str(x) for x in self.fila])
-        return f"Caixas: [{caixas}]\nFila: [{fila}]"
+        return f"Caixas: [{caixas}]\nEspera: [{fila}]"
     
     def map(self, lista: list):
         ls: list = []
@@ -38,28 +38,28 @@ class Mercantil:
 
     def chamar(self, index: int):
 
-        if self.caixas is not None:
-            print("Caixa ocupado")
+        if self.caixas[index] is not None:
+            print("fail: caixa ocupado")
             return
 
         elif len(self.fila) == 0:
-            print("Fila vazia")
+            print("fail: fila vazia")
             return
 
         elif index > 0 or index >= len(self.caixas):
-            print("index invalido")
+            print("fail: caixa inexistente")
             return
 
         self.caixas[index] = self.fila[0]
         del self.fila[0]
 
-    def finalizar(self, index: int) -> Cliente | None:
-        if index > 0 or index >= len(self.caixas):
-            print("caixa inexistente")
+    def finish(self, index: int) -> Cliente | None:
+        if index < 0 or index >= len(self.caixas):
+            print("fail: caixa inexistente")
             return None
 
         elif self.caixas is None:
-            print("Caixa vazio")
+            print("fail: caixa vazio")
             return None
             
         aux = self.caixas[index]
@@ -68,10 +68,9 @@ class Mercantil:
         return aux
         
 def main():
-    mercantil = Mercantil | None = None
+    mercantil: Mercantil | None = None
     
     while True:
-        
         try:
             line = input()
             args = line.split()
@@ -88,17 +87,17 @@ def main():
             elif cmd == "show":
                 print(mercantil)
 
-            elif cmd == "enter":
+            elif cmd == "arrive":
                 mercantil.chegar(Cliente(args[1]))
             
             elif cmd == "call":
                 mercantil.chamar(int(args[1]))
 
             elif cmd == "finish":
-                cliente = mercantil.finish(int(args[1]))
-                if cliente:
-                    print(f"Cliente {cliente.getNome()} finalizou compra")
+                mercantil.finish(int(args[1]))
+                
         except EOFError:
+
             break
 
 main()
